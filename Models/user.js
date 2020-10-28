b83c146106997446554004f18a677a56d8fdd9a3
Mads <mads>
2020-10-28 12:01:19 +0100

    Opsætning af HTML og mongodb

--- Models/user.js
+++ Models/user.js
@@ -1,2 +1,15 @@
-var jens = 2
-var mads = "mit navn er mads"
+class User{
+    constructor(userId, firstName, lastName, age, gender, email, password, location, interests, image){
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.passwrod = password;
+        this.location = location;
+        this.interests = interests;
+        this.image = image;
+    };
+    
+};
---
+++ script.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://Madsriber:<Telefon6>@eksamensprojekt.rpi3d.mongodb.net/<Eksamensprojekt>?retryWrites=true&w=majority", { useNewUrlParser = true, useUnifiedTopology = true
+});
